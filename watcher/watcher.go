ea8f6ba7c0e6f158453b75b65d20228f37c7beeb
Anirudha Tambolkar <atambol@ncsu.edu>
2018-09-16 18:11:01 -0400

    tracking online players

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -5,20 +5,29 @@ import (
 	"fmt"
 	"github.com/samuel/go-zookeeper/zk"
 	"os"
+	"strconv"
 	"time"
 )
 
-func exitIfError(err error, msg string, player string) {
-	if err != nil {
-		fmt.Printf("%s %s. %s\n", player, msg, err)
-		//panic(err)
+func main() {
+	// Initialize data structures
+	var isOnline map[string]bool
+
+	//server := "127.0.0.1:2181"
+	// Parse arguments
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Printf("Expected 2 arguments, got %d\n", len(args))
 		os.Exit(1)
 	}
-}
 
-func main() {
-	player := "Watcher"
-	server := "127.0.0.1:2181"
+	server := args[0]
+	displaySize, err := strconv.Atoi(args[1])
+	utils.ExitIfError(err, "Could not convert size to integer.")
+	if displaySize > 25 || displaySize < 1 {
+		fmt.Printf("Display size expected in range [1, 25], got %d", displaySize)
+		os.Exit(1)
+	}
 
 	/* ********************** Register with server ****************** */
 	// Connect to server
@@ -39,20 +48,36 @@ func main() {
 		fmt.Println("Scoreboard directory created")
 	}
 
-	/* ********************* Keep Watching *************************** */
-	children, _, err := conn.Children(utils.ScoreDir)
-	exitIfError(err, "Could not get children", player)
-	fmt.Printf("here 3")
-	for _, name := range children {
-		znodePath := utils.GetZnodePath(utils.ScoreDir, name)
-		fmt.Println(znodePath)
-		data, _, err := conn.Get(znodePath)
-		exitIfError(err, "could not data from " + znodePath, player)
-		fmt.Printf("%s stored at %s \n", data, znodePath)
-		//err = conn.Delete("/dir/" + name, 0)
+	/* ********************* Track online players *************************** */
+	for true {
+		children, _, ech, err := conn.ChildrenW(utils.OnlineDir)
+		utils.ExitIfError(err, "Could not watch for online players")
+		isOnline := make(map[string]bool)
+		for _, name := range children {
+			isOnline[name] = true
+		}
+		_ = <-ech
 	}
+
+	/* ********************* Keep Watching for scores *************************** */
+	//for true {
+	//	children, _, err := conn.Children(utils.ScoreDir)
+	//	utils.ExitIfError(err, "Could not get children")
+	//
+	//	for _, name := range children {
+	//		znodePath := utils.GetZnodePath(utils.ScoreDir, name)
+	//		fmt.Println(znodePath)
+	//		data, _, err := conn.Get(znodePath)
+	//		utils.ExitIfError(err, "Could not data from "+znodePath)
+	//		fmt.Printf("%s stored at %s \n", data, znodePath)
+	//		//err = conn.Delete("/dir/" + name, 0)
+	//	}
+	//	time.Sleep(500 * time.Millisecond)
+	//}
 	// Create a data structure to store score and online presence data
 	// Get initial tree structure, set watches
 	// Create a display function
 	// Display the data and update on watch
 }
+
+
